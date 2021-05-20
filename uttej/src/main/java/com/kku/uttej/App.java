62c4006fa644edf0d54515733c25eeca427b7fa1
Home <Home@DESKTOP-6R0COV2>
2021-05-20 17:44:33 +0530

    commit kku

---
+++ uttej/src/main/java/com/kku/uttej/App.java
@@ -0,0 +1,13 @@
+package com.kku.uttej;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
